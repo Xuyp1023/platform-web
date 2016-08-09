548b7fa9c7f3a02a7046022f11333027e7ef5fc1
root <git4bytter@sina.cn>
2016-08-09 16:46:50 +0800

    task-087-ex01

--- src/main/java/com/betterjr/modules/customer/CustOpenAccountController.java
+++ src/main/java/com/betterjr/modules/customer/CustOpenAccountController.java
@@ -12,11 +12,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.dubbo.rpc.RpcException;
+import com.betterjr.common.exception.BytterException;
 import com.betterjr.common.web.AjaxObject;
 import com.betterjr.common.web.Servlets;
 
 @Controller
-@RequestMapping("/Platform/CustOpenAccount")
+@RequestMapping("/Platform/Account")
 public class CustOpenAccountController {
 
     private static final Logger logger = LoggerFactory.getLogger(CustOpenAccountController.class);
@@ -24,71 +26,165 @@ public class CustOpenAccountController {
     @Reference(interfaceClass = ICustOpenAccountService.class)
     private ICustOpenAccountService custOpenAccountService;
 
-    @RequestMapping(value = "/openAccount", method = RequestMethod.POST)
-    public @ResponseBody String openAccount(HttpServletRequest request, Long id, String fileList) {
+    @RequestMapping(value = "/findAccInfo", method = RequestMethod.POST)
+    public @ResponseBody String findOpenAccountInfo() {
+        logger.info("客户开户资料读取");
+        try {
+
+            return custOpenAccountService.webFindOpenAccountInfo();
+        }
+        catch (RpcException e) {
+            if (e.getCause() != null && e.getCause() instanceof BytterException) {
+                return AjaxObject.newError(e.getCause().getMessage()).toJson();
+            }
+            logger.error("客户开户资料读取失败", e);
+            return AjaxObject.newError("客户开户资料读取失败").toJson();
+        }
+        catch (Exception e) {
+            logger.error("客户开户资料读取失败", e);
+            return AjaxObject.newError("客户开户资料读取失败").toJson();
+        }
+    }
+
+    @RequestMapping(value = "/saveAccInfo", method = RequestMethod.POST)
+    public @ResponseBody String saveOpenAccountInfo(HttpServletRequest request) {
         Map<String, Object> anMap = Servlets.getParametersStartingWith(request, "");
-        logger.info("开户资料,入参：" + anMap.toString());
+        logger.info("客户开户资料暂存,入参：" + anMap.toString());
         try {
 
-            return custOpenAccountService.webSaveOpenAccount(anMap, id, fileList);
+            return custOpenAccountService.webSaveOpenAccountInfo(anMap);
+        }
+        catch (RpcException e) {
+            if (e.getCause() != null && e.getCause() instanceof BytterException) {
+                return AjaxObject.newError(e.getCause().getMessage()).toJson();
+            }
+            logger.error("客户开户资料暂存失败", e);
+            return AjaxObject.newError("客户开户资料暂存失败").toJson();
         }
         catch (Exception e) {
-            logger.error("开户失败", e);
-            return AjaxObject.newError("开户失败").toJson();
+            logger.error("客户开户资料暂存失败", e);
+            return AjaxObject.newError("客户开户资料暂存失败").toJson();
         }
     }
 
-    @RequestMapping(value = "/findOpenAccountTemp", method = RequestMethod.POST)
-    public @ResponseBody String findOpenAccountTemp() {
-        logger.info("开户资料读取");
-        try {
-
-            return custOpenAccountService.webFindOpenAccountTemp();
-        }
-        catch (Exception e) {
-            logger.error("开户资料读取失败", e);
-            return AjaxObject.newError("开户资料读取失败").toJson();
-        }
-    }
-
-    @RequestMapping(value = "/findOpenAccountTempByInsteadId", method = RequestMethod.POST)
-    public @ResponseBody String findOpenAccountTempByInsteadId(Long id) {
-        logger.info("开户资料读取,入参：" + id);
-        try {
-
-            return custOpenAccountService.webFindOpenAccountTempByInsteadId(id);
-        }
-        catch (Exception e) {
-            logger.error("开户资料读取失败", e);
-            return AjaxObject.newError("开户资料读取失败").toJson();
-        }
-    }
-
-    @RequestMapping(value = "/saveOpenAccountTemp", method = RequestMethod.POST)
-    public @ResponseBody String saveOpenAccountTemp(HttpServletRequest request, String fileList) {
+    @RequestMapping(value = "/saveAccApply", method = RequestMethod.POST)
+    public @ResponseBody String saveOpenAccountApply(HttpServletRequest request, Long id) {
         Map<String, Object> anMap = Servlets.getParametersStartingWith(request, "");
-        logger.info("开户资料暂存,入参：" + anMap.toString());
+        logger.info("开户申请,入参：" + anMap.toString());
         try {
 
-            return custOpenAccountService.webSaveOpenAccountTemp(anMap, fileList);
+            return custOpenAccountService.webSaveOpenAccountApply(anMap, id);
+        }
+        catch (RpcException e) {
+            if (e.getCause() != null && e.getCause() instanceof BytterException) {
+                return AjaxObject.newError(e.getCause().getMessage()).toJson();
+            }
+            logger.error("开户申请提交失败", e);
+            return AjaxObject.newError("开户申请提交失败").toJson();
         }
         catch (Exception e) {
-            logger.error("开户资料暂存失败", e);
-            return AjaxObject.newError("开户资料暂存失败").toJson();
+            logger.error("开户申请提交失败", e);
+            return AjaxObject.newError("开户申请提交失败").toJson();
         }
     }
 
-    @RequestMapping(value = "/saveOpenAccountInsteadTemp", method = RequestMethod.POST)
-    public @ResponseBody String saveOpenAccountInsteadTemp(HttpServletRequest request, Long id, String fileList) {
-        Map<String, Object> anMap = Servlets.getParametersStartingWith(request, "");
-        logger.info("开户资料暂存,入参：" + anMap.toString());
+    @RequestMapping(value = "/queryAccApply", method = RequestMethod.POST)
+    public @ResponseBody String queryOpenAccountApply(String flag, int pageNum, int pageSize) {
         try {
 
-            return custOpenAccountService.webSaveOpenAccountInsteadTemp(anMap, id, fileList);
+            return custOpenAccountService.webQueryOpenAccountApply(flag, pageNum, pageSize);
+        }
+        catch (RpcException e) {
+            if (e.getCause() != null && e.getCause() instanceof BytterException) {
+                return AjaxObject.newError(e.getCause().getMessage()).toJson();
+            }
+            logger.error("开户申请待审批列表查询失败", e);
+            return AjaxObject.newError("开户申请待审批列表查询失败").toJson();
         }
         catch (Exception e) {
-            logger.error("开户资料暂存失败", e);
-            return AjaxObject.newError("开户资料暂存失败").toJson();
+            logger.error("开户申请待审批列表查询失败", e);
+            return AjaxObject.newError("开户申请待审批列表查询失败").toJson();
+        }
+    }
+
+    @RequestMapping(value = "/saveAuditccApply", method = RequestMethod.POST)
+    public @ResponseBody String saveAuditOpenAccountApply(Long id, String auditOpinion) {
+        logger.info("开户审核,入参：" + id + " and " + auditOpinion);
+        try {
+
+            return custOpenAccountService.webSaveAuditOpenAccountApply(id, auditOpinion);
+        }
+        catch (RpcException e) {
+            if (e.getCause() != null && e.getCause() instanceof BytterException) {
+                return AjaxObject.newError(e.getCause().getMessage()).toJson();
+            }
+            logger.error("开户审核失败", e);
+            return AjaxObject.newError("开户审核失败").toJson();
+        }
+        catch (Exception e) {
+            logger.error("开户审核失败", e);
+            return AjaxObject.newError("开户审核失败").toJson();
+        }
+    }
+
+    @RequestMapping(value = "/saveRefuseAccApply", method = RequestMethod.POST)
+    public @ResponseBody String saveRefuseOpenAccountApply(Long id, String auditOpinion) {
+        logger.info("开户申请驳回,入参：" + id + " and " + auditOpinion);
+        try {
+
+            return custOpenAccountService.webSaveRefuseOpenAccountApply(id, auditOpinion);
+        }
+        catch (RpcException e) {
+            if (e.getCause() != null && e.getCause() instanceof BytterException) {
+                return AjaxObject.newError(e.getCause().getMessage()).toJson();
+            }
+            logger.error("开户申请驳回失败", e);
+            return AjaxObject.newError("开户申请驳回失败").toJson();
+        }
+        catch (Exception e) {
+            logger.error("开户申请驳回失败", e);
+            return AjaxObject.newError("开户申请驳回失败").toJson();
+        }
+    }
+
+    @RequestMapping(value = "/saveAccInfoInstead", method = RequestMethod.POST)
+    public @ResponseBody String saveOpenAccountInfoByInstead(HttpServletRequest request, Long insteadId) {
+        Map<String, Object> anMap = Servlets.getParametersStartingWith(request, "");
+        logger.info("代录开户资料暂存,入参：" + anMap.toString());
+        try {
+
+            return custOpenAccountService.webSaveOpenAccountInfoByInstead(anMap, insteadId);
+        }
+        catch (RpcException e) {
+            if (e.getCause() != null && e.getCause() instanceof BytterException) {
+                return AjaxObject.newError(e.getCause().getMessage()).toJson();
+            }
+            logger.error("代录开户资料暂存失败", e);
+            return AjaxObject.newError("代录开户资料暂存失败").toJson();
+        }
+        catch (Exception e) {
+            logger.error("代录开户资料暂存失败", e);
+            return AjaxObject.newError("代录开户资料暂存失败").toJson();
+        }
+    }
+
+    @RequestMapping(value = "/findAccInfoInstead", method = RequestMethod.POST)
+    public @ResponseBody String findOpenAccountInfoByInsteadId(Long insteadId) {
+        logger.info("代录开户资料读取,入参：" + insteadId);
+        try {
+
+            return custOpenAccountService.webFindOpenAccountInfoByInsteadId(insteadId);
+        }
+        catch (RpcException e) {
+            if (e.getCause() != null && e.getCause() instanceof BytterException) {
+                return AjaxObject.newError(e.getCause().getMessage()).toJson();
+            }
+            logger.error("代录开户资料读取失败", e);
+            return AjaxObject.newError("代录开户资料读取失败").toJson();
+        }
+        catch (Exception e) {
+            logger.error("代录开户资料读取失败", e);
+            return AjaxObject.newError("代录开户资料读取失败").toJson();
         }
     }
 
