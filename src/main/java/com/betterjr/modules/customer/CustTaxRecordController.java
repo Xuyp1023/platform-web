3501a2de9636faabfd65a58bfc1b0cc374d12b8e
wudy <wudy@bytter.com>
2016-08-15 16:50:45 +0800

    纳税信息模块

---
+++ src/main/java/com/betterjr/modules/customer/CustTaxRecordController.java
@@ -0,0 +1,87 @@
+package com.betterjr.modules.customer;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.alibaba.dubbo.config.annotation.Reference;
+import com.alibaba.dubbo.rpc.RpcException;
+import com.betterjr.common.exception.BytterException;
+import com.betterjr.common.web.AjaxObject;
+import com.betterjr.common.web.Servlets;
+
+@Controller
+@RequestMapping("/Platform/CustTaxRecord")
+public class CustTaxRecordController {
+
+    private static final Logger logger = LoggerFactory.getLogger(CustTaxRecordController.class);
+    
+    @Reference(interfaceClass = ICustTaxService.class)
+    private ICustTaxService custTaxService;
+
+    @RequestMapping(value = "/queryCustTaxRecord", method = RequestMethod.POST)
+    public @ResponseBody String queryCustTaxRecord(Long custNo, String flag, int pageNum, int pageSize) {
+        logger.info(",入参:custNo=" + custNo);
+        try {
+            return custTaxService.webQueryTaxRecordList(custNo, flag, pageSize, pageNum);
+        }
+        catch (RpcException btEx) {
+            logger.error(btEx.getMessage(), btEx);
+            if (BytterException.isCauseBytterException(btEx)) {
+                return AjaxObject.newError(btEx.getCause().getMessage()).toJson();
+            }
+            return AjaxObject.newError("纳税信息查询失败").toJson();
+        }
+        catch (Exception ex) {
+            logger.error(ex.getMessage(), ex);
+            return AjaxObject.newError("纳税信息查询失败").toJson();
+        }
+    }
+    
+    @RequestMapping(value = "/addCustTaxRecord", method = RequestMethod.POST)
+    public @ResponseBody String addCustTaxRecord(HttpServletRequest request, String fileList) {
+        Map<String, Object> anMap = Servlets.getParametersStartingWith(request, "");
+        logger.info("纳税信息录入,入参:" + anMap.toString());
+        try {
+            return custTaxService.webAddTaxRecord(anMap, fileList);
+        }
+        catch (RpcException btEx) {
+            logger.error(btEx.getMessage(), btEx);
+            if (BytterException.isCauseBytterException(btEx)) {
+                return AjaxObject.newError(btEx.getCause().getMessage()).toJson();
+            }
+            return AjaxObject.newError("纳税信息录入失败").toJson();
+        }
+        catch (Exception ex) {
+            logger.error(ex.getMessage(), ex);
+            return AjaxObject.newError("纳税信息录入失败").toJson();
+        }
+    }
+    
+    @RequestMapping(value = "/saveCustTaxRecord", method = RequestMethod.POST)
+    public @ResponseBody String saveCustTaxRecord(HttpServletRequest request, Long id, String fileList) {
+        Map<String, Object> anMap = Servlets.getParametersStartingWith(request, "");
+        logger.info("纳税信息录入,入参:" + anMap.toString());
+        try {
+            return custTaxService.webSaveTaxRecord(anMap, id, fileList);
+        }
+        catch (RpcException btEx) {
+            logger.error(btEx.getMessage(), btEx);
+            if (BytterException.isCauseBytterException(btEx)) {
+                return AjaxObject.newError(btEx.getCause().getMessage()).toJson();
+            }
+            return AjaxObject.newError("纳税信息保存失败").toJson();
+        }
+        catch (Exception ex) {
+            logger.error(ex.getMessage(), ex);
+            return AjaxObject.newError("纳税信息保存失败").toJson();
+        }
+    }
+}
