dd765f0ca4673495fba51f90c6c1076b88f21dd7
wudy <wudy@bytter.com>
2016-12-07 20:17:01 +0800

    开户代码合并优化

--- src/main/java/com/betterjr/modules/customer/CustOpenAccountController.java
+++ src/main/java/com/betterjr/modules/customer/CustOpenAccountController.java
@@ -1,5 +1,7 @@
 package com.betterjr.modules.customer;
 
+import static com.betterjr.common.web.ControllerExceptionHandler.exec;
+
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -247,5 +249,134 @@ public class CustOpenAccountController {
             return AjaxObject.newError("开户审批流程查询失败").toJson();
         }
     }
+    
+    /**
+     * 检查申请机构名称是否存在
+     */
+    @RequestMapping(value = "/checkCustExistsByCustName", method = RequestMethod.POST)
+    public @ResponseBody String checkCustExistsByCustName(String custName) {
+        logger.info("检查申请机构名称是否存在,入参: " + custName);
+        return exec(() -> custOpenAccountService.webCheckCustExistsByCustName(custName), "检查申请机构名称是否存在失败", logger);
+    }
+    
+    /**
+     * 检查组织机构代码证是否存在
+     */
+    @RequestMapping(value = "/checkCustExistsByIdentNo", method = RequestMethod.POST)
+    public @ResponseBody String checkCustExistsByIdentNo(String identNo){
+        logger.info("检查组织机构代码证是否存在,入参: " + identNo);
+        return exec(() -> custOpenAccountService.webCheckCustExistsByIdentNo(identNo), "检查组织机构代码证是否存在失败", logger);
+    }
+    
+    /**
+     * 检查营业执照号码是否存在
+     */
+    @RequestMapping(value = "/checkCustExistsByBusinLicence", method = RequestMethod.POST)
+    public @ResponseBody String checkCustExistsByBusinLicence(String businLicence) {
+        logger.info("检查营业执照号码是否存在,入参: " + businLicence);
+        return exec(() -> custOpenAccountService.webCheckCustExistsByBusinLicence(businLicence), "检查营业执照号码是否存在失败", logger);
+    }
+    
+    /**
+     * 检查银行账号是否存在
+     */
+    @RequestMapping(value = "/checkCustExistsByBankAccount", method = RequestMethod.POST)
+    public @ResponseBody String checkCustExistsByBankAccount(String bankAccount) {
+        logger.info("检查银行账号是否存在,入参: " + bankAccount);
+        return exec(() -> custOpenAccountService.webCheckCustExistsByBankAccount(bankAccount), "检查银行账号是否存在失败", logger);
+    }
+    
+    /**
+     * 检查电子邮箱是否存在
+     */
+    @RequestMapping(value = "/checkCustExistsByEmail", method = RequestMethod.POST)
+    public @ResponseBody String checkCustExistsByEmail(String email) {
+        logger.info("检查电子邮箱是否存在,入参: " + email);
+        return exec(() -> custOpenAccountService.webCheckCustExistsByEmail(email), "检查电子邮箱是否存在失败", logger);
+    }
+    
+    /**
+     * 检查银行账号是否存在
+     */
+    @RequestMapping(value = "/checkCustExistsByMobileNo", method = RequestMethod.POST)
+    public @ResponseBody String checkCustExistsByMobileNo(String mobileNo) {
+        logger.info("检查手机号码是否存在,入参: " + mobileNo);
+        return exec(() -> custOpenAccountService.webCheckCustExistsByMobileNo(mobileNo), "检查手机号码是否存在失败", logger);
+    }
+    
+    /**
+     * 开户申请提交
+     */
+    @RequestMapping(value = "/saveOpenAccountApplySubmit", method = RequestMethod.POST)
+    public @ResponseBody String saveOpenAccountApplySubmit(HttpServletRequest request, Long operId, String fileList) {
+        Map<String, Object> anMap = Servlets.getParametersStartingWith(request, "");
+        logger.info("开户申请提交,入参: " + anMap.toString());
+        return exec(() -> custOpenAccountService.webSaveOpenAccountApplySubmit(anMap, operId, fileList), "开户申请提交失败", logger);
+    }
 
+    /**
+     * PC端使用，根据operOrg查询insteadApply的业务状态
+     */
+    @RequestMapping(value = "/findInsteadApplyStatus", method = RequestMethod.POST)
+    public @ResponseBody String findInsteadApplyStatus() {
+        return exec(() -> custOpenAccountService.webFindInsteadApplyStatus(), "状态查询失败", logger);
+    }
+    
+    /**
+     * PC端使用，根据operOrg查询开户信息
+     */
+    @RequestMapping(value = "/findOpenAccoutnTmp", method = RequestMethod.POST)
+    public @ResponseBody String findOpenAccoutnTmp() {
+        return exec(() -> custOpenAccountService.webFindOpenAccoutnTmp(), "信息查询失败", logger);
+    }
+    
+    
+    /**
+     * 微信查询开户资料
+     */
+    @RequestMapping(value = "/findAccountTmpInfo", method = RequestMethod.POST)
+    public @ResponseBody String findAccountTmpInfo() {
+        try {
+            final Object openIdObj = Servlets.getSession().getAttribute("wechat_openId");
+            if (openIdObj != null) {
+                final String openId = String.valueOf(openIdObj);
+                return custOpenAccountService.webFindAccountTmpInfo(openId);
+            }
+            return AjaxObject.newError("获取开户信息失败").toJson();
+        }
+        catch (final Exception e) {
+            return AjaxObject.newError("获取开户信息失败").toJson();
+        }
+    }
+    
+    /**
+     * 开户资料附件保存
+     */
+    @RequestMapping(value = "/saveSingleFileLink", method = RequestMethod.POST)
+    public @ResponseBody String saveSingleFileLink(Long id, final String fileTypeName, final String fileMediaId) {
+        return exec(() -> custOpenAccountService.webSaveSingleFileLink(id, fileTypeName, fileMediaId), "开户资料附件保存", logger);
+    }
+    
+    /**
+     * 根据batchNo生成对应文件类型Map Json对象(微信使用)
+     */
+    @RequestMapping(value = "/findAccountFileByBatChNo", method = RequestMethod.POST)
+    public @ResponseBody String findAccountFileByBatChNo(Long batchNo) {
+        logger.info("附件查询,入参：" + batchNo );
+        try {
+
+            return custOpenAccountService.webFindAccountFileByBatChNo(batchNo);
+        }
+        catch (RpcException e) {
+            logger.error(e.getMessage(), e);
+            if (BytterException.isCauseBytterException(e)) {
+                return AjaxObject.newError(e.getCause().getMessage()).toJson();
+            }
+            return AjaxObject.newError("附件查询失败").toJson();
+        }
+        catch (Exception e) {
+            logger.error(e.getMessage(), e);
+            return AjaxObject.newError("附件查询失败").toJson();
+        }
+    }
 }
