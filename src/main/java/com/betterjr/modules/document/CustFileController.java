d1452e47ae6b98c5475cc85a78c256e82f4d4616
wudy <wudy@bytter.com>
2016-12-12 17:28:59 +0800

    文件打包

--- src/main/java/com/betterjr/modules/document/CustFileController.java
+++ src/main/java/com/betterjr/modules/document/CustFileController.java
@@ -1,6 +1,5 @@
 package com.betterjr.modules.document;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,8 +17,6 @@ import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import com.alibaba.dubbo.config.annotation.Reference;
-import com.betterjr.common.config.ParamNames;
-import com.betterjr.common.utils.BetterStringUtils;
 import com.betterjr.common.utils.FileUtils;
 import com.betterjr.common.web.AjaxObject;
 import com.betterjr.modules.document.entity.CustFileItem;
@@ -50,6 +47,20 @@ public class CustFileController {
         try {
             CustFileItem fileItem = fileItemService.findOne(id);
             FileWebClientUtils.fileDownload(storeService, response, fileItem);
+        }
+        catch (Exception ex) {
+            logger.error(ex.getMessage(), ex);
+        }
+    }
+    
+    /**
+     * 文件批量下载
+     */
+    @RequestMapping(value = "/fileMultipleDownload")
+    public @ResponseBody void fileMultipleDownload(String fileIdList, HttpServletResponse response, String fileName) {
+        try {
+            List<CustFileItem> fileItemList = fileItemService.findFileListByIds(fileIdList.split(","));
+            FileWebClientUtils.fileMultipleDownload(storeService, response, fileItemList, fileName);
         }
         catch (Exception ex) {
             logger.error(ex.getMessage(), ex);
