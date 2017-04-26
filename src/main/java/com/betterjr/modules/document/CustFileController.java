c793a9aa9e4036bc452dd794439b7a2dc3aba76e
Xuyp <xuyp@bytter.com>
2017-04-26 16:48:46 +0800

    文件上传

--- src/main/java/com/betterjr/modules/document/CustFileController.java
+++ src/main/java/com/betterjr/modules/document/CustFileController.java
@@ -3,10 +3,10 @@ package com.betterjr.modules.document;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.apache.commons.lang3.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +24,7 @@ import com.betterjr.common.web.AjaxObject;
 import com.betterjr.common.web.ControllerExceptionHandler;
 import com.betterjr.common.web.ControllerExceptionHandler.ExceptionHandler;
 import com.betterjr.modules.document.entity.CustFileItem;
+import com.betterjr.modules.document.entity.CustResolveFile;
 import com.betterjr.modules.document.service.DataStoreService;
 import com.betterjr.modules.document.utils.FileWebClientUtils;
 
@@ -60,6 +61,66 @@ public class CustFileController {
         }
     }
     
+    /**
+     * 创建解析文件的日志，并且通过消息解析文件
+     * @param request
+     * @param custNo
+     * @param coreCustNo
+     * @param id
+     * @param infoType 要解析的文件的类型   解析的文件类型1订单 2票据  3应收账款 4发票 5 合同
+     * @return
+     */
+    @RequestMapping(value = "/saveResolveFile", method = RequestMethod.POST)
+    public @ResponseBody String saveResolveFile(HttpServletRequest request, Long custNo,Long coreCustNo,Long id,String infoType) {
+        
+        try{
+            logger.info("文件解析添加日志记录,入参：" + custNo.toString()+" "+coreCustNo.toString()+"   "+id.toString());
+            
+            CustFileItem fileItem = fileItemService.findOneAndButchId(id);
+            if(fileItem ==null || StringUtils.isBlank(fileItem.getFileType())){
+                return AjaxObject.newError("文件解析日志插入失败,日志").toJson();
+            }
+            if( ! (fileItem.getFileType().equals("xls") ||fileItem.getFileType().equals("xlsx"))){
+                return AjaxObject.newError("文件解析失败，解析的文件应该是excel").toJson();
+            }
+            CustResolveFile resolveFile=new CustResolveFile();
+            resolveFile.setBusinStatus("0");
+            resolveFile.setCoreCustNo(coreCustNo);
+            resolveFile.setCustNo(custNo);
+            resolveFile.setFileItemId(id);
+            resolveFile.setBatchNo(fileItem.getBatchNo());
+            resolveFile.setFileLength(fileItem.getFileLength());
+            resolveFile.setFileType(fileItem.getFileType());
+            resolveFile.setInfoType(infoType);
+            resolveFile=fileItemService.webSaveAddResolveFile(resolveFile);
+            //发送MQ消息
+            Long timetemp=System.currentTimeMillis();
+            logger.info("文件解析添加日志记录,发送消息开始时间"+timetemp );
+            boolean flag=sendResolveMessage(resolveFile);
+            logger.info("文件解析添加日志记录,发送消息结束时间时间"+System.currentTimeMillis()+"   一共耗时："+(System.currentTimeMillis()-timetemp)+"" );
+            if(flag){
+                return AjaxObject.newOk("文件解析日志插入成功", resolveFile).toJson();
+            }else{
+                return AjaxObject.newError("文件解析日志插入成功,但消息发送失败,日志ID："+resolveFile.getId()).toJson();
+            }
+            
+        }catch(Exception ex){
+            logger.error(ex.getMessage(), "文件解析添加日志记录"+ex);
+            return AjaxObject.newError("文件解析失败!!!").toJson();
+        }
+        
+    }
+    
+    /**
+     * 发送解析文件的类型
+     * @param anResolveFile
+     */
+    private boolean sendResolveMessage(CustResolveFile anResolveFile) {
+        
+       return fileItemService.sendResolveMessage(anResolveFile);
+        
+    }
+
     /**
      * 文件批量下载
      */
